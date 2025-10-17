48a19272951719b8d9fac5b7e2a49c8383b1d320
Горбунов Александр <qwaeszrdxtfc1@yandex.ru>
2025-10-18 01:05:09 +0700

    add funс mask_account_card

---
+++ src/widget.py
@@ -0,0 +1,18 @@
+from masks import get_mask_card_number, get_mask_account
+
+def mask_account_card(requisites: str) -> str:
+    """Возвращает маску номера счета или карты"""
+
+    requisites_number = ''
+    requisites_type = ''
+    for i in requisites:
+        if i.isdigit():
+            requisites_number += i
+        elif i.isalpha() or i == ' ':
+            requisites_type += i
+    if 'счет' in requisites_type.lower() or 'счёт' in requisites_type.lower():
+        requisites_mask = 'Счет ' + get_mask_account(requisites_number)
+    else:
+        requisites_type = requisites_type.strip()
+        requisites_mask = requisites_type + ' ' + get_mask_card_number(requisites_number)
+    return requisites_mask
