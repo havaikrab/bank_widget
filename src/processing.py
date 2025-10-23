c8cf83933738d266eec70e55dfc77c6bb8934343
Горбунов Александр <qwaeszrdxtfc1@yandex.ru>
2025-10-23 19:41:09 +0700

    linters fixes

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,7 @@
 def filter_by_state(dicts_list: list, state: str = "EXECUTED") -> list:
     '''Возвращает список словарей с указанным значением state из передаваемого списка словарей,
     по умолчанию state = "EXECUTED"'''
+
     filtred_list = []
     for dict in dicts_list:
         if dict.get("state") == state:
@@ -11,5 +12,6 @@ def filter_by_state(dicts_list: list, state: str = "EXECUTED") -> list:
 def sort_by_date(dicts_list: list, sort_reverse: bool = True) -> list:
     """Возвращает передаваемый список словарей, отсортированным по дате,
     по умолчанию в обратном хронологическом порядке"""
+
     sorted_list = sorted(dicts_list, key=lambda dict: dict["date"], reverse=sort_reverse)
     return sorted_list
