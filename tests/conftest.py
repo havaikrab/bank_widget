2fd931e8267e7cdb4b4d469162525e708916ea18
Горбунов Александр <qwaeszrdxtfc1@yandex.ru>
2025-11-04 18:12:42 +0700

    add several fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,3 +4,11 @@ import pytest
 @pytest.fixture
 def card_number_standart():
     return 1234567890098765
+
+@pytest.fixture
+def card_number_short():
+    return 9876543210
+
+@pytest.fixture
+def card_number_long():
+    return 122333444455555666666
