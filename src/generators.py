24d0c018f1085d4f82817f7152d0ed2885f2a465
Горбунов Александр <qwaeszrdxtfc1@yandex.ru>
2025-11-12 14:23:20 +0700

    add tests/test_generators.py

--- src/generators.py
+++ src/generators.py
@@ -1,6 +1,9 @@
 def filter_by_currency(transactions: list, currency: str) -> list:
     """Возвращает спсок словарей-транзакций с указанной валютой"""
 
+    if not isinstance(transactions, list):
+        raise TypeError("Некорректный формат источника данных")
+
     filtered_transactions = (transaction for transaction in transactions if
                              transaction["operationAmount"]["currency"]["name"] == currency)
 
