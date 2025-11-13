cdcf2c6801f96eab237ebeeb1962b333c75c8416
Горбунов Александр <qwaeszrdxtfc1@yandex.ru>
2025-11-13 12:18:36 +0700

    add tests for filter_by_currency

--- src/generators.py
+++ src/generators.py
@@ -1,11 +1,16 @@
-def filter_by_currency(transactions: list, currency_code: str) -> dict:
+from typing import Iterator, List
+
+
+def filter_by_currency(transactions: List[dict], currency_code: str) -> Iterator:
     """Возвращает спсок словарей-транзакций с указанной валютой"""
 
-    if not isinstance(transactions, list):
-        raise TypeError("Некорректный формат источника данных")
 
-    filtered_transactions = (transaction for transaction in transactions if
-                             transaction["operationAmount"]["currency"]["code"] == currency_code)
+    filtered_transactions = (transaction for transaction in transactions
+                             if isinstance(transaction, dict)
+                             and "operationAmount" in transaction
+                             and "currency" in transaction["operationAmount"]
+                             and "code" in transaction["operationAmount"]["currency"]
+                             and transaction["operationAmount"]["currency"]["code"] == currency_code)
 
     for transaction in filtered_transactions:
         yield transaction
