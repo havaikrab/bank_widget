1b3f709865b0e8c08a294c804b80b566df3d8ffe
Горбунов Александр <qwaeszrdxtfc1@yandex.ru>
2025-12-24 03:23:38 +0700

    add func xlsx_data_reader

--- src/data_readers.py
+++ src/data_readers.py
@@ -18,3 +18,21 @@ def csv_data_reader(path_to_file: str) -> list:
 
     return sorted_reader.to_dict(orient='records')
 
+
+def xlsx_data_reader(path_to_file: str) -> list:
+    '''Возвращает список отформатированных словарей-транзакций из передаваемого xlsx-файла'''
+
+    reader = pd.read_excel(path_to_file)
+
+    currency = reader.loc[:, ['currency_name', 'currency_code']]
+    currency.columns = ['name', 'code']
+    currency_list = currency.to_dict(orient='records')
+    reader = reader.assign(currency=currency_list)
+
+    operation_amount = reader.loc[:, ['amount', 'currency']]
+    operation_amount_list = operation_amount.to_dict(orient='records')
+    reader = reader.assign(operationAmount=operation_amount_list)
+
+    sorted_reader = reader.loc[:, ['id', 'state', 'date', 'operationAmount', 'description', 'from', 'to']]
+
+    return sorted_reader.to_dict(orient='records')
