e64813a718fa6b291082f0d8f3f15b5f15532e08
Горбунов Александр <qwaeszrdxtfc1@yandex.ru>
2025-12-28 19:15:51 +0700

    add func aggregate_by_description with tests

--- src/searchers.py
+++ src/searchers.py
@@ -1,14 +1,28 @@
 import re
+from collections import Counter
+
 
 def find_description(transactions_list: list, text: str) -> list:
-    """Возвращает список словарей-транзакций по заданному описанию"""
+    """Возвращает список словарей-транзакций, в описании которых есть слова из передаваемой строки"""
 
-    pattern = re.compile(r'\b\w+\b')
+    pattern = re.compile(r"\b\w+\b")
     key_words = re.findall(pattern, text)
     required_list = []
     for transaction in transactions_list:
         for word in key_words:
-            if word.lower() in transaction.get('description', '').lower():
+            if word.lower() in transaction.get("description", "").lower():
                 required_list.append(transaction)
                 break
     return required_list
+
+
+def aggregate_by_description(transactions_list: list, requirements_list: list):
+    """Возвращает словарь, содержащий названия категорий транзакций с количеством вхождений соответствующей категории в список словарей-транзакций"""
+
+    descriptions = (
+        transaction.get("description")
+        for transaction in transactions_list
+        if transaction.get("description") in requirements_list
+    )
+    counted_descriptions = Counter(descriptions)
+    return dict(counted_descriptions)
